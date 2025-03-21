6a065eb8720ff15270b6c7d22ea947bb9e8002c3
Killian KOPP <killiankopp@gmail.com>
2025-03-21 13:29:27 +0100

    uri connexion via secret

--- mongodb/create-secret.sh
+++ mongodb/create-secret.sh
@@ -1,3 +1,3 @@
-kubectl create secret generic mongodb-koden-meow-secret -n karned \
+kubectl create secret generic mongodb-koden-meow-secret -n koden \
   --from-literal=mongodb-root-password="supermotdepasse" \
   --from-literal=mongodb-passwords="autresupermotdepasse"
